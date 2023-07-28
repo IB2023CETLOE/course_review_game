d9c9085a8399c979d8e5139a37f2021875109e26
IB2023CETLOE <140831681+IB2023CETLOE@users.noreply.github.com>
2023-07-28 13:00:45 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,68 +1,60 @@
-
 let questions = [
-    // ... Your questions, answers, and points go here ...
+    {
+        question: "Have you provided students with information regarding workload, course-specific fees, and any requirements for in-person or synchronous attendance before the start of the semester?",
+        answers: [
+            {text: "No, I haven't provided this information.", points: 0},
+            {text: "I have provided some but not all of this information.", points: 1},
+            {text: "Yes, I have provided all of this information.", points: 2}
+        ]
+    },
+    {
+        question: "Does the syllabus meet all necessary criteria?",
+        answers: [
+            {text: "No, the syllabus is incomplete or missing elements.", points: 0},
+            {text: "The syllabus meets most criteria, but could use improvement.", points: 1},
+            {text: "Yes, the syllabus meets all criteria.", points: 2}
+        ]
+    },
+    // ... Continue with similar structure for the other checklist items ...
 ];
 
 let currentQuestion = 0;
 let totalScore = 0;
 
-// Find elements on the page
 let questionText = document.getElementById('question-text');
 let answerButtons = Array.from(document.getElementsByClassName('answer-button'));
 let resultsContainer = document.getElementById('results-container');
 let scoreText = document.getElementById('score-text');
 let recommendationText = document.getElementById('recommendation-text');
 
-// Function to display a question
 function displayQuestion() {
-    // Set the question text
     questionText.textContent = questions[currentQuestion].question;
-
-    // Set the answer button texts
     for (let i = 0; i < answerButtons.length; i++) {
         answerButtons[i].textContent = questions[currentQuestion].answers[i].text;
     }
 }
 
-// Add click event listeners to the answer buttons
 for (let i = 0; i < answerButtons.length; i++) {
     answerButtons[i].addEventListener('click', function() {
         handleAnswer(i);
     });
 }
 
-// Function to handle a user's answer
 function handleAnswer(answerIndex) {
-    // Add the points for the selected answer to the total score
     totalScore += questions[currentQuestion].answers[answerIndex].points;
-
-    // Move on to the next question
     currentQuestion++;
-
-    // If there are more questions, display the next one
     if (currentQuestion < questions.length) {
         displayQuestion();
     } else {
-        // Otherwise, display the total score and recommendations
         displayResults();
     }
 }
 
-// Function to display the total score and recommendations
 function displayResults() {
-    // Hide the quiz container
     document.getElementById('quiz-container').style.display = 'none';
-
-    // Show the results container
     resultsContainer.style.display = 'block';
-
-    // Calculate the average score per question
     let averageScore = totalScore / questions.length;
-
-    // Display the total score
     scoreText.textContent = totalScore;
-
-    // Display a recommendation based on the average score
     if (averageScore < 1) {
         recommendationText.textContent = "Your course design needs significant improvement. Consider revisiting the checklist items and implementing more of the recommendations.";
     } else if (averageScore < 2) {
@@ -72,5 +64,4 @@ function displayResults() {
     }
 }
 
-// Start the quiz by displaying the first question
 displayQuestion();
